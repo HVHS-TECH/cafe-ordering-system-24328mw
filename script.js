ade83f59c7dc0ff217e4befd91a2ccb50cfb0eda
24328mw <24328mw@hvhs.school.nz>
2026-06-11 21:33:44 +0000

    Jun 11, 2026, 9:33 PM

--- script.js
+++ script.js
@@ -21,4 +21,4 @@ const menuItems = [
         price: 27
     }
 ]
-MENU_OUTPUT.innerHTML = "<img src=menuItems[0].image.src alt=menuItems[0].name>"
+MENU_OUTPUT.innerHTML = "<img src=evaporatedWater.png alt=menuItems[0].name>"
