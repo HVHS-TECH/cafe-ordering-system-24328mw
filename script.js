88ce7766ca2ec22dd369dae2c5ecb0c1d19426e3
24328mw <24328mw@hvhs.school.nz>
2026-06-21 22:18:08 +0000

    Jun 21, 2026, 10:18 PM

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ console.log("Hello world!");
 //link to the HTML page for the outputs
 const MENU_OUTPUT = document.getElementById("menuOutput");
 const ORDER_FORM_OUTPUT = document.getElementById("orderFormOutput");
-
 //store menu items in an object
 const menuItems = [
     {
@@ -19,16 +18,32 @@ const menuItems = [
         price: 27
     }
 ]
+//add an array for the user's order
+let cartArray = []
 //use a function to display the menu items
 function displayMenuItem(_name, _price, _imageSrc){
-    MENU_OUTPUT.innerHTML += "<img src="+_imageSrc+" alt="+_name+" width=150>"
-    MENU_OUTPUT.innerHTML += "<p>"+_name+"</p>"
+    MENU_OUTPUT.innerHTML += "<img src="+_imageSrc+" alt="+_name+" width=150>";
+    MENU_OUTPUT.innerHTML += "<p>"+_name+"</p>";
 }
 //function to open the order form
 function openOrder(){
-    ORDER_FORM_OUTPUT.innerHTML
+    ORDER_FORM_OUTPUT.innerHTML = "<h4>Name:</h4>";
+    ORDER_FORM_OUTPUT.innerHTML += "<form id=nameForm onsubmit='return false'><input id=nameField type=text required><input type=submit onclick=getNameFormInput()></form>";
+    ORDER_FORM_OUTPUT.innerHTML += "<h4>Add an item to your cart:</h4>";
+    ORDER_FORM_OUTPUT.innerHTML += "<form id=itemForm onsubmit='return false'><input id=evaporatedWater type=radio name=menuOption value=water><label for=water>Evaporated water</label><br><input id=deconstructedCake type=radio name=menuOption value=cake><label for=cake>Deconstruced cake</label><br><input id=agedMilkMilkshake type=radio name=menuOption value=milkshake><label for=milkshake>Aged milk milkshake</label><br><input type=submit onclick=getItemFormInput></form>";
+    ORDER_FORM_OUTPUT.innerHTML += "<h4>Your Cart:</h4>";
+    for (let i=0; i<cartArray.length; i++){
+        ORDER_FORM_OUTPUT.innerHTML += "<p>"+menuItems[cartArray[i]].name+"</p>";
+    }
+    ORDER_FORM_OUTPUT.innerHTML += "<button onclick=placeOrder>Place Order</button>"
 }
 //use a for loop to display full menu
 for (let i=0; i<menuItems.length; i++){
-    displayMenuItem(menuItems[i].name, menuItems[i].price, menuItems[0].imageSrc)
+    displayMenuItem(menuItems[i].name, menuItems[i].price, menuItems[0].imageSrc);
+}
+
+
+function getNameFormInput(){
+}
+function getItemFormInput(){
 }
