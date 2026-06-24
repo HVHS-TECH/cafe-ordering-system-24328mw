9b0cd698bfcd02f2a847706b9c471ac2cdfab319
24328mw <24328mw@hvhs.school.nz>
2026-06-24 02:28:32 +0000

    Jun 24, 2026, 2:28 AM

--- script.js
+++ script.js
@@ -37,7 +37,9 @@ function openOrder(){
     NAME_FORM_OUTPUT.innerHTML += "<form id=nameForm onsubmit='return false'><input id=nameField type=text required><input type=submit onclick=getNameFormInput()></form>";
     ITEM_FORM_OUTPUT.innerHTML += "<h4>Add an item to your cart:</h4>";
     ITEM_FORM_OUTPUT.innerHTML += "<form id=itemForm onsubmit='return false'><input id=evaporatedWater type=radio name=menuOption value=water><label for=water>Evaporated water</label><br><input id=deconstructedCake type=radio name=menuOption value=cake><label for=cake>Deconstruced cake</label><br><input id=agedMilkMilkshake type=radio name=menuOption value=milkshake><label for=milkshake>Aged milk milkshake</label><br><input type=submit onclick=getItemFormInput()></form>";
-    CART_OUTPUT.innerHTML = "<h4>Your Cart:</h4>";
+    CART_OUTPUT.innerHTML += "<h4>Your Cart:</h4>";
+    MONEY_FORM_OUTPUT.innerHTML += "<h4>Please enter your money</h4>"
+    NAME_FORM_OUTPUT.innerHTML += "<form id=nameForm onsubmit='return false'><input id=nameField type=number required><input type=submit onclick=getMoneyFormInput()></form>";
     ORDER_BUTTON_OUTPUT.innerHTML += "<button onclick=placeOrder>Place Order</button>"
 }
 //use a for loop to display full menu
