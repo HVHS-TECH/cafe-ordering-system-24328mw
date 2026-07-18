744c393c0d27e91d1525acb5ae42d51282913188
24328mw <24328mw@hvhs.school.nz>
2026-07-18 23:37:29 +0000

    Jul 18, 2026, 11:37 PM

--- script.js
+++ script.js
@@ -119,9 +119,7 @@ function placeOrder(){
     }
     else if (confirm("Please confirm your name is "+USER.name) == true){
         if (confirm("Please confirm you have $"+USER.money+" to pay with") == true){
-            let fullOrder = menuItems[cartArray].name;
-            ORDER_BUTTON_OUTPUT.innerHTML = "<p>"+fullOrder+"</p>"
-            if (confirm("Please confirm your order is "+USER.name) == true){
+            if (confirm("Please confirm your order is: ") == true){
                 completeOrder()
             }
         }
