aad6c56e930c6ecdd93321c854bc1f443fe2575c
24328mw <24328mw@hvhs.school.nz>
2026-07-18 23:11:17 +0000

    Jul 18, 2026, 11:11 PM

--- script.js
+++ script.js
@@ -124,7 +124,7 @@ function placeOrder(){
         ORDER_BUTTON_OUTPUT.innerHTML += "<p>Add an item to you cart</p>";
     }
     else if (confirm("Please confirm your name is "+USER.name) == true){
-        if (confirm("Please confirm you have "+USER.money+" to pay with") == true){
+        if (confirm("Please confirm you have $"+USER.money+" to pay with") == true){
 
         }
     }
